12bc69c3ab4fea34ba5dd2f0edb2f089d353f880
wirbel <womp@herr-der-mails.de>
2023-03-23 08:53:33 +0100

    update to 2023

--- unit1.pas
+++ unit1.pas
@@ -13,12 +13,15 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
+    CheckBox_VAT: TCheckBox;
     Edit_Count: TEdit;
     Edit_width: TEdit;
     Edit_height: TEdit;
     GroupBox1: TGroupBox;
     Label1: TLabel;
     Label4: TLabel;
+    Label_Beauti_HD6L: TLabel;
+    Label9: TLabel;
     Label_Beauti_HD2L: TLabel;
     Label2: TLabel;
     Label3: TLabel;
@@ -50,7 +53,10 @@ implementation
 {$R *.lfm}
 
 const
-   {prices in euro, 22.03.2023 }
+   {prices in euro (excl VAT),
+    22.03.2023
+    https://community.aisler.net/t/our-simple-pricing/102
+    }
    Beauti_Budget2L_JobFee:double = 12.0;  { 2022: 6.0  }
    Beauti_Budget2L_sqcm:double   = 0.042;
    Beauti_Blitz2L_JobFee:double  = 16.0;  { 2022: 12.0 }
@@ -79,6 +85,7 @@ const
    Beauti_Blitz2L_PCB:Integer       = 2;
    Beauti_HD2L_PCB:Integer          = 5;
    Beauti_HD4L_PCB:Integer          = 7;
+   Beauti_HD6L_PCB:Integer          = 7;
    General_Dispatch:Integer         = 1;
    Shipping_Regular_DE:Integer      = 2;
    Shipping_Regular_DE_UPS:Integer  = 1;
@@ -91,6 +98,7 @@ var w,h:double;
     n:integer;
     days,shipping:Integer;
     fs:TFormatSettings;
+    sum,tmp:double;
 begin
   fs:=FormatSettings;
   fs.DecimalSeparator:='.';
@@ -102,17 +110,52 @@ begin
   while(n mod Beauti_multiple) <> 0 do
      inc(n);
 
+  { shipping time }
   if RadioButton2.Checked then        shipping := Shipping_Regular_DE_UPS
   else if RadioButton3.Checked then   shipping := Shipping_Regular_Int
   else if RadioButton4.Checked then   shipping := Shipping_Regular_Int_UPS
   else                                shipping := Shipping_Regular_DE;
-
   days := General_Dispatch + shipping;
 
-  Label_Beauti_Budget2L.Caption := FloatToStrF((1.0+VAT_DE)*(Beauti_Budget2L_JobFee + (w*h)*0.01*Beauti_Budget2L_sqcm*n),ffFixed,10,2) + '€ (' + IntToStr(n) + ' boards, ' + IntToStr(days + Beauti_Budget2L_PCB) + ' working days)';
-  Label_Beauti_Blitz2L.Caption  := FloatToStrF((1.0+VAT_DE)*(Beauti_Blitz2L_JobFee  + (w*h)*0.01*Beauti_Blitz2L_sqcm *n),ffFixed,10,2) + '€ (' + IntToStr(n) + ' boards, ' + IntToStr(days + Beauti_Blitz2L_PCB ) + ' working days)';
-  Label_Beauti_HD2L.Caption     := FloatToStrF((1.0+VAT_DE)*(Beauti_HD2L_JobFee     + (w*h)*0.01*Beauti_HD2L_sqcm    *n),ffFixed,10,2) + '€ (' + IntToStr(n) + ' boards, ' + IntToStr(days + Beauti_HD2L_PCB    ) + ' working days)';
-  Label_Beauti_HD4L.Caption     := FloatToStrF((1.0+VAT_DE)*(Beauti_HD4L_JobFee     + (w*h)*0.01*Beauti_HD4L_sqcm    *n),ffFixed,10,2) + '€ (' + IntToStr(n) + ' boards, ' + IntToStr(days + Beauti_HD4L_PCB    ) + ' working days)';
+  { shipping costs }
+  if RadioButton2.Checked then        tmp := Shipping_UPS_costs
+  else if RadioButton4.Checked then   tmp := Shipping_UPS_express_costs
+  else                                tmp:=0; { default: free shipping. }
+
+  { beauty 2-layer}
+  sum := tmp;
+  sum += Beauti_Budget2L_JobFee + (w*h)*0.01*Beauti_Budget2L_sqcm * n;
+  if CheckBox_VAT.checked then
+     sum *= (1.0 + VAT_DE);
+  Label_Beauti_Budget2L.Caption := FloatToStrF(sum,ffFixed,10,2) + '€ (' + IntToStr(n) + ' boards, ' + IntToStr(days + Beauti_Budget2L_PCB) + ' working days)';
+
+  { beauty 2-layer express}
+  sum := tmp;
+  sum += Beauti_Blitz2L_JobFee + (w*h)*0.01*Beauti_Blitz2L_sqcm * n;
+  if CheckBox_VAT.checked then
+     sum *= (1.0 + VAT_DE);
+  Label_Beauti_Blitz2L.Caption := FloatToStrF(sum,ffFixed,10,2) + '€ (' + IntToStr(n) + ' boards, ' + IntToStr(days + Beauti_Blitz2L_PCB) + ' working days)';
+
+  { beauty HD 2-layer}
+  sum := tmp;
+  sum += Beauti_HD2L_JobFee + (w*h)*0.01*Beauti_HD2L_sqcm * n;
+  if CheckBox_VAT.checked then
+     sum *= (1.0 + VAT_DE);
+  Label_Beauti_HD2L.Caption := FloatToStrF(sum,ffFixed,10,2) + '€ (' + IntToStr(n) + ' boards, ' + IntToStr(days + Beauti_HD2L_PCB) + ' working days)';
+
+  { beauty HD 4-layer}
+  sum := tmp;
+  sum += Beauti_HD4L_JobFee     + (w*h)*0.01*Beauti_HD4L_sqcm * n;
+  if CheckBox_VAT.checked then
+     sum *= (1.0 + VAT_DE);
+  Label_Beauti_HD4L.Caption := FloatToStrF(sum,ffFixed,10,2) + '€ (' + IntToStr(n) + ' boards, ' + IntToStr(days + Beauti_HD4L_PCB) + ' working days)';
+
+  { beauty HD 6-layer}
+  sum := tmp;
+  sum += Beauti_HD6L_JobFee     + (w*h)*0.01*Beauti_HD6L_sqcm * n;
+  if CheckBox_VAT.checked then
+     sum *= (1.0 + VAT_DE);
+  Label_Beauti_HD6L.Caption := FloatToStrF(sum,ffFixed,10,2) + '€ (' + IntToStr(n) + ' boards, ' + IntToStr(days + Beauti_HD6L_PCB) + ' working days)';
 
 end;
 
@@ -123,9 +166,9 @@ begin
   memo1.lines.add('');
   memo1.lines.Add('Selecting shipping other that default may cause extra costs.');
   memo1.lines.add('');
-  memo1.lines.add('Beautiful Budget 2L: width:0.20,  spacing:0.15  drill=0.3, milling:2.4, slots:n');
-  memo1.lines.add('Beautiful Blitz  2L: width:0.20,  spacing:0.15  drill=0.3, milling:2.4, slots:n');
-  memo1.lines.add('Beautiful HD  2L/4L: width:0.125, spacing:0.125 drill=0.2, milling:1.6, slots:y');
+  memo1.lines.add('Beautiful Budget 2L   : width:0.20,  spacing:0.15  drill=0.3, milling:2.4, slots:n');
+  memo1.lines.add('Beautiful Blitz  2L   : width:0.20,  spacing:0.15  drill=0.3, milling:2.4, slots:n');
+  memo1.lines.add('Beautiful HD  2L/4L/6L: width:0.125, spacing:0.125 drill=0.2, milling:1.6, slots:y');
   memo1.ReadOnly:=true;
 
 
